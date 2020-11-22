1d61c5c40dcfe8f6ddf3b1bdf1769bbacbc73419
Lorenzo Bulosan Bulosan <Lorenzo.bulosan@yahoo.co.uk>
2020-11-22 02:05:09 +0000

    feature to change the color of nodes depending on button currently active

--- js/index.js
+++ js/index.js
@@ -7,13 +7,18 @@ function main(){
 // sets graph element to specific class
 $('.element').click(function(){
 
-	// read button selected
+	var activeButton = $('.btnGraph.active');
+	var valueOfActiveButton = activeButton.val();
 
+	// read button selected and change properties
+	if(activeButton.length==0){
+		alert('No node style selected!');
+	}else{
 
-	// change style
-	$(this).hide();
+		resetStartStopNode();
+		$(this).addClass(valueOfActiveButton);
 
-	// add class
+	}
 });
 
 // sets button to active and removes the active states from others
@@ -21,6 +26,19 @@ $('.btnGraph').click(function setActive(){
 	$('.btnGraph').removeClass('active');
 	$(this).addClass('active');
 });
+
+// checks that there's a single starting/target node
+function resetStartStopNode(){
+
+	var startNodes = $('.element.startNode');
+	var targetNodes = $('.element.targetNode');
+
+	console.log([startNodes.length,targetNodes.length])
+
+	if(startNodes.length == 1){$('.element').removeClass('startNode');}
+	if(targetNodes.length == 1){$('.element').removeClass('targetNode');}
+
+}
 
 // debugger functions
 function popsAlert(){
